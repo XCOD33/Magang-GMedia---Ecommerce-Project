b2fefe479f1913ad3bf98c26126fd2967f3c4911
XCOD33 <denistraju155@gmail.com>
2025-02-12 15:46:43 +0700

    ✨ (web.php): menambahkan route untuk middleware superadmin dan seller 📝 (web.php): mengubah tampilan halaman utama untuk menampilkan error session Perubahan ini menambahkan grup route untuk middleware superadmin dan seller, yang memungkinkan pengelompokan rute berdasarkan peran pengguna. Selain itu, halaman utama sekarang menampilkan pesan error dari session, meningkatkan pengalaman pengguna dengan memberikan umpan balik yang lebih baik.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Home " . session('error') ?? '';
+});
+
+Route::group([
+    'middleware' => 'superadmin',
+    'as' => 'superadmin.',
+    'prefix' => 'superadmin',
+], function () {
+    // middleware superadmin
+});
+
+Route::group([
+    'middleware' => 'seller',
+    'as' => 'seller.',
+    'prefix' => 'seller',
+], function () {
+    // middleware seller
 });
