6a8b96fa008984040b522d27ba6451fbd21110cf
XCOD33 <denistraju155@gmail.com>
2025-02-19 10:08:40 +0700

    ✨ (AppServiceProvider.php, SlugService.php, web.php): menambahkan SlugService dan memperbarui rute Menambahkan SlugService untuk memudahkan pembuatan slug yang ramah URL dan unik. Pembaruan pada rute menambahkan grup rute untuk otentikasi dan dashboard, yang memisahkan logika rute berdasarkan peran pengguna. Ini meningkatkan struktur dan keamanan aplikasi dengan mengelompokkan rute berdasarkan middleware yang sesuai.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\SlugService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        /**
+         * Registers the SlugService as a singleton in the application's service container.
+         * This allows the SlugService to be injected and used throughout the application.
+         */
+        $this->app->singleton(SlugService::class, function () {
+            return new SlugService();
+        });
     }
 
     /**
